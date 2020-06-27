7238c3af1c0204076e036ad3734175ac874e9287
Quyse Lert <quyse0@gmail.com>
2020-06-27 18:53:45 +0300

    simplify nix expression a bit

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
 { nixpkgs }:
 rec {
   packages = nixpkgs.haskellPackages.override {
-    overrides = let
-      sourceOverrides = nixpkgs.haskell.lib.packageSourceOverrides {
+    overrides = with nixpkgs.haskell.lib; let
+      sourceOverrides = packageSourceOverrides {
         messdb = ./messdb;
         messdb-schema = ./messdb-schema;
         messdb-sqlite = ./messdb-sqlite;
       };
 
-      deps = self: super: with nixpkgs.haskell.lib; {
+      deps = self: super: {
         messdb-sqlite = overrideCabal super.messdb-sqlite (attrs: {
           librarySystemDepends = [ nixpkgs.sqlite ];
         });
