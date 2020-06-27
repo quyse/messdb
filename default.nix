cbb0f5b771695248b7ae9c39ffc43af81943bd00
Quyse Lert <quyse0@gmail.com>
2020-06-27 18:50:28 +0300

    fix messdb-sqlite deps in nix

--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
 { nixpkgs }:
 rec {
   packages = nixpkgs.haskellPackages.override {
-    overrides = nixpkgs.haskell.lib.packageSourceOverrides {
-      messdb = ./messdb;
-      messdb-schema = ./messdb-schema;
-      messdb-sqlite = ./messdb-sqlite;
-    };
+    overrides = let
+      sourceOverrides = nixpkgs.haskell.lib.packageSourceOverrides {
+        messdb = ./messdb;
+        messdb-schema = ./messdb-schema;
+        messdb-sqlite = ./messdb-sqlite;
+      };
+
+      deps = self: super: with nixpkgs.haskell.lib; {
+        messdb-sqlite = overrideCabal super.messdb-sqlite (attrs: {
+          librarySystemDepends = [ nixpkgs.sqlite ];
+        });
+      };
+
+    in nixpkgs.lib.composeExtensions sourceOverrides deps;
   };
 }
